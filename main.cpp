c9ab240cb4d860ba28c8434524e0e9d33cb29be5
Mikhail <merzlickinm@yandex.ru>
2024-12-10 08:42:59 +0300

    была добавлена сумма чисел

--- main.cpp
+++ main.cpp
@@ -7,8 +7,11 @@ int main() {
     std::cout << "Chisla cherez probel: ";
     std::cin >> num1 >> num2;
 
+    int sum = num1 + num2;
+
     std::cout << "1 chislo: " << num1 << std::endl;
     std::cout << "2 chislo: " << num2 << std::endl;
+    std::cout << "Sum: " << sum << std::endl;
 
     return 0;
 }
