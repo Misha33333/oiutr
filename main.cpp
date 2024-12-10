24740215667522bfe427efca8c6e6a5506c75de7
Mikhail <merzlickinm@yandex.ru>
2024-12-10 08:59:18 +0300

    добавлена функция умножения

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@ int main() {
     std::cin >> num1 >> num2;
 
     int razn = num1 - num2;
-
+    int y = num1 * num2;
     std::cout << "1 chislo: " << num1 << std::endl;
     std::cout << "2 chislo: " << num2 << std::endl;
     std::cout << "razn: " << razn << std::endl;
-
+    /*ààààà*/
     return 0;
 }
