06740bc9cecffdc394b2bc1e335753a7d8da4b9f
Mikhail <merzlickinm@yandex.ru>
2024-12-10 08:51:44 +0300

    добавлена разность

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ int main() {
     std::cout << "Chisla cherez probel: ";
     std::cin >> num1 >> num2;
 
-    int sum = num1 + num2;
+    int razn = num1 - num2;
 
     std::cout << "1 chislo: " << num1 << std::endl;
     std::cout << "2 chislo: " << num2 << std::endl;
-    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "razn: " << razn << std::endl;
 
     return 0;
 }
