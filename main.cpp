c17352073bcb94a9f4829ede1ce860781767c9e3
Mikhail <merzlickinm@yandex.ru>
2024-12-10 08:40:45 +0300

    первоначальная версия программы

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "std.h"
+
+int main() {
+    int num1, num2;
+
+    std::cout << "Chisla cherez probel: ";
+    std::cin >> num1 >> num2;
+
+    std::cout << "1 chislo: " << num1 << std::endl;
+    std::cout << "2 chislo: " << num2 << std::endl;
+
+    return 0;
+}
